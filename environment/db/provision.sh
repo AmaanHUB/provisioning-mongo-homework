178eab54cd05c6debc05d063cfc86cc81b71b709
Amaan Hashmi-Ubhi <AmaanHUB@gmail.com>
2020-11-24 19:37:11 +0000

    Finished provision, all tests pass

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -4,13 +4,14 @@
 sudo apt update && sudo apt upgrade
 
 # Install MongoDB, is not in default Ubuntu, cannot get the 3.2.20 version too
-curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt update
-sudo apt install mongodb-org -y
+sudo apt install mongodb-org=3.2.20 -y
 
-# Start the server and enable
+# Change the IP for MongoDB to 0.0.0.0
+sudo sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
+# Start the server and enable with the changes made
 sudo systemctl enable mongod.service --now
-
-#
 
